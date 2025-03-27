faf5be54d74437a405745e8b3ae78390de25ecc9
David G Huss <david.huss@gmail.com>
2025-03-27 14:36:15 -0400

    fixes

--- src/main/java/frc/robot/library/subsystems/vision/localizationfusion/util/LocalizationState.java
+++ src/main/java/frc/robot/library/subsystems/vision/localizationfusion/util/LocalizationState.java
@@ -10,7 +10,7 @@
 *
 * Source code must be publicly available on GitHub or an alternative web accessible site
 */
-package frc.alotobots.library.subsystems.vision.localizationfusion.util;
+package frc.robot.library.subsystems.vision.localizationfusion.util;
 
 import lombok.Getter;
 
